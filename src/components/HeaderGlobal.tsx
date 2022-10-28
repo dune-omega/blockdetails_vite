f0e4db3047932ad65e8647a1d4d24ad5219054ca
WF\omega <jsomega@itcom888.com>
2022-10-28 15:15:14 +0800

    UPDATE:
    1. Added table on home page.
    2. get coin list from API
    3. added more changes on headerGlobal

--- src/components/HeaderGlobal.tsx
+++ src/components/HeaderGlobal.tsx
@@ -1,5 +1,5 @@
-import { Select, Space } from "antd";
-import React from "react";
+import { Input, Select, Space } from "antd";
+import { useNavigate } from "react-router-dom";
 import { currencyArr, symbol } from "../constants";
 import { GlobalState } from "../context/GlobalContext";
 import { TSymbol, TTotalMarketCap } from "../types";
@@ -14,48 +14,54 @@ const HeaderGlobal = () => {
     setCurrency(value);
   };
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <Space align="center">
-        <div>
-          <span>Cryptos: </span>
-          <span>{global?.active_cryptocurrencies || "-"}</span>
-        </div>
-        <div>
-          <span>Market Cap: </span>
-          <span>
-            {symbol[currency as keyof TSymbol]}
-            {global?.total_market_cap[currency as keyof TTotalMarketCap] || "-"}
-          </span>
-        </div>
-        <div>
-          <span>Volume: </span>
-          <span>
-            {symbol[currency as keyof TSymbol]}
-            {global?.total_volume[currency as keyof TTotalMarketCap] || "-"}
-          </span>
-        </div>
+      <Space>
+        <Space align="center">
+          <div>
+            <span>Cryptos: </span>
+            <span>{global?.active_cryptocurrencies || "-"}</span>
+          </div>
+          <div>
+            <span>Market Cap: </span>
+            <span>
+              {symbol[currency as keyof TSymbol]}
+              {global?.total_market_cap[currency as keyof TTotalMarketCap] ||
+                "-"}
+            </span>
+          </div>
+          <div>
+            <span>Volume: </span>
+            <span>
+              {symbol[currency as keyof TSymbol]}
+              {global?.total_volume[currency as keyof TTotalMarketCap] || "-"}
+            </span>
+          </div>
+        </Space>
+        <Select
+          showSearch
+          onChange={changeSearchCurrency}
+          onSearch={changeSearchCurrency}
+          filterOption={(input, option) =>
+            (option!.children as unknown as string)
+              .toLowerCase()
+              .includes(input.toLowerCase())
+          }
+          size="middle"
+          placeholder="Select a currency: "
+          style={{ width: 200 }}
+          defaultValue={"usd"}
+        >
+          {currencyArr.map((symbol) => (
+            <Option value={symbol.value} key={symbol.name}>
+              {symbol.name}
+            </Option>
+          ))}
+        </Select>
+        <Input placeholder="Basic usage" />
       </Space>
-      <Select
-        showSearch
-        onChange={changeSearchCurrency}
-        onSearch={changeSearchCurrency}
-        filterOption={(input, option) =>
-          (option!.children as unknown as string)
-            .toLowerCase()
-            .includes(input.toLowerCase())
-        }
-        size="middle"
-        placeholder="Select a currency: "
-        style={{ width: 200 }}
-        defaultValue={"usd"}
-      >
-        {currencyArr.map((symbol) => (
-          <Option value={symbol.value} key={symbol.name}>
-            {symbol.name}
-          </Option>
-        ))}
-      </Select>
     </>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,67 @@
+import { ProColumns, ProTable } from "@ant-design/pro-components";
+import { Space } from "antd";
+import { useRef, useState } from "react";
+import { API_COIN_LIST } from "../../api/http";
+import { GlobalState } from "../../context/GlobalContext";
+import { useFetchAPISingle } from "../../hooks/useFetchAPISingle";
 import "./index.module.scss";
 
+export type TTableCoin = {
+  image: string;
+  name: string;
+  market_cap_rank: number;
+};
+
 const Home = () => {
+  const actionRef = useRef();
+  const { currency } = GlobalState();
+  const [page, setPage] = useState(1);
+  const [pageSize, setPagesize] = useState(100);
+  const columns: ProColumns<TTableCoin>[] = [
+    {
+      title: "#",
+      dataIndex: "market_cap_rank",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (_, record) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <img src={record.image} width={20} />
+          <span>{record.name}</span>
+        </div>
+      ),
+    },
+  ];
+
+  const { data: coins } = useFetchAPISingle(
+    API_COIN_LIST(currency, pageSize, page)
+  );
+
+  console.log(coins);
+
   return (
     <>
-      <div style={{ display: "flex", gap: "1rem" }}>hello World</div>
+      <ProTable
+        search={false}
+        options={false}
+        columns={columns}
+        dataSource={coins}
+        actionRef={actionRef}
+        pagination={{
+          onChange: (page, pageSize) => {
+            setPage(page);
+            setPagesize(pageSize);
+          },
+        }}
+      />
     </>
   );
 };
